8d6102e7b4e9ba87f4618b025a3f7e9dbdbb02d9
chris-carpenter <carpycarp44@gmail.com>
2022-12-10 00:18:36 -0500

    Exercise 1, 2 Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnString = "";
+    for (let x = 1; x <= num; x++){
+        returnString += string;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
