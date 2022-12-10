4b966be5cf1b67cec063cd0922d1b6e696e2bf8e
chris-carpenter <carpycarp44@gmail.com>
2022-12-10 17:40:38 -0500

    Reverse String exercise solved. Improvements poss

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
+    for (let x = string.length; x >= 0; x--){
+        reversedString += string.charAt(x);
+    }
 
+    return reversedString;
 };
 
 // Do not edit below this line
