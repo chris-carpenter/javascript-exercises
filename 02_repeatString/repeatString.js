a0306ed79748020745623d39df24cec3a7e7c06e
chris-carpenter <carpycarp44@gmail.com>
2022-12-10 19:53:36 -0500

    Fixed repeatString exercise to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,15 @@
 const repeatString = function(string, num) {
     let returnString = "";
-    for (let x = 1; x <= num; x++){
-        returnString += string;
+    if (num >= 0){
+        for (let x = 1; x <= num; x++){
+            returnString += string;
+        }
+        return returnString;
     }
-    return returnString;
+    else {
+        return "ERROR"
+    }
+    
 };
 
 // Do not edit below this line
