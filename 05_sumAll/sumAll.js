29acd7e11d7ae28709eb55edb5c0122cdc62e4f6
chris-carpenter <carpycarp44@gmail.com>
2022-12-11 02:53:36 -0500

    Finished the sumAll function problem.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
 const sumAll = function() {
-
+    if (arguments[0] < 0 || arguments[1] < 0){
+        return "ERROR";
+    }
+    else if (typeof(arguments[0]) != "number" || typeof(arguments[1]) != "number"){
+        return "ERROR";
+    }
+    else {
+        let bottomNum = Math.min(arguments[0], arguments[1]);
+        let topNum = Math.max(arguments[0], arguments[1]);
+        let finalSum = 0;
+        
+        for (let x = bottomNum; x <= topNum; x++){
+            finalSum += x;
+        }
+        return finalSum;
+    }
+    
 };
 
 // Do not edit below this line
